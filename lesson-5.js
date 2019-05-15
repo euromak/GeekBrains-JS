e4d3d6cf9bfcf8e3fa84f6693780174693350553
euromak <euromak2016@yandex.ru>
2019-05-15 17:49:47 +0300

    Added some code for exersise # 1

--- lesson-5.js
+++ lesson-5.js
@@ -2,19 +2,50 @@ var $table = document.createElement('div');
 $table.classList.add('table');
 document.body.appendChild($table);
 
+var $row = document.createElement('div');
+$row.classList.add('row');
+
 var $cell = document.createElement('div');
-$cell.innerHTML = 'T';
+$cell.classList.add('cell');
 
-
-
-for(var i = 0; i < 64; i++){
-    $cell = $cell.cloneNode(true);
-    $table.appendChild($cell);
-    $cell.classList.add('cell');
+for(var i = 0; i < 8; i++){
+    $table.appendChild($row.cloneNode($row));
+    for(var j = 0; j < 8; j++){
+        var rowCell = document.getElementsByClassName('row')[i];
+        rowCell.appendChild($cell.cloneNode($cell));
+    }
 }
-/*
-for(var i = 0; i < 64; i++){
-    $cell.classList.add('black');
+
+for(i = 0; i < 8; i++){
+    var rowCell = document.getElementsByClassName('row')[i];
+    var cellColor = document.getElementsByClassName('cell');
+
+    if(i % 2 == 1){
+        cellColor[i].style.backgroundColor = '#7e4105';
+    }
     console.log(i);
+
+}
+
+
+
+
+/*
+// добавляем клетки в ряд
+for(var i = 0; i < 8; i++){
+    $cell = $cell.cloneNode(true);
+    $row.appendChild($cell);
+}
+// добавляем ряды в логический контейнер
+for(i = 0; i < 8; i++){
+    $row = $row.cloneNode(true);
+    $table.appendChild($row);
 }
 */
+// раскрашиваем клетки доски
+
+
+
+
+
+
