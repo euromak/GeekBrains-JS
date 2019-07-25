b2a6c64db7a10b258a8882afcde6a108ee945276
euromak <euromak2016@yandex.ru>
2019-07-25 22:50:31 +0300

    Added styles

--- project/js/main.js
+++ project/js/main.js
@@ -1,19 +1,19 @@
 const products = [
-  {id: 1, title: 'Notebook', price: 1000, image: 'https://www.topmangal.com/wp-content/uploads/man/product_1.jpg',},
-  {id: 2, title: 'Mouse', price: 100, image: 'https://www.topmangal.com/wp-content/uploads/man/product_2.jpg',},
-  {id: 3, title: 'Keyboard', price: 250, image: 'https://www.topmangal.com/wp-content/uploads/man/product_3.jpg',},
-  {id: 4, title: 'Gamepad', price: 150, image: 'https://www.topmangal.com/wp-content/uploads/man/product_4.jpg',},
-  {id: 5, title: 'Gamepad', price: 150, image: 'https://www.topmangal.com/wp-content/uploads/man/product_5.jpg',},
-  {id: 6, title: 'Gamepad', price: 150, image: 'https://www.topmangal.com/wp-content/uploads/man/product_6.jpg',},
-  {id: 7, title: 'Gamepad', price: 150, image: 'https://www.topmangal.com/wp-content/uploads/man/product_7.jpg',},
-  {id: 8, title: 'Gamepad', price: 150, image: 'https://www.topmangal.com/wp-content/uploads/man/product_8.jpg',},
+  {id: 1, title: 'Куртка', price: 10000, image: 'https://www.topmangal.com/wp-content/uploads/man/product_1.jpg',},
+  {id: 2, title: 'Пальто', price: 10000, image: 'https://www.topmangal.com/wp-content/uploads/man/product_2.jpg',},
+  {id: 3, title: 'Куртка', price: 7500, image: 'https://www.topmangal.com/wp-content/uploads/man/product_3.jpg',},
+  {id: 4, title: 'Поло', price: 4500, image: 'https://www.topmangal.com/wp-content/uploads/man/product_4.jpg',},
+  {id: 5, title: 'Худи', price: 6000, image: 'https://www.topmangal.com/wp-content/uploads/man/product_5.jpg',},
+  {id: 6, title: 'Куртка', price: 7000, image: 'https://www.topmangal.com/wp-content/uploads/man/product_6.jpg',},
+  {id: 7, title: 'Пиджак', price: 5500, image: 'https://www.topmangal.com/wp-content/uploads/man/product_7.jpg',},
+  {id: 8, title: 'Куртка', price: 9000, image: 'https://www.topmangal.com/wp-content/uploads/man/product_8.jpg',},
 ];
 
 const renderProduct = (title, price, image) => {
   return `<div class="product-item">
             <img src="${image}" alt="">
             <h3>${title}</h3>
-            <p>${price}</p>
+            <p>${price} руб.</p>
             <button class="by-btn">Добавить</button>
           </div>`;
 };
