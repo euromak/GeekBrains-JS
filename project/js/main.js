e7b4e91abbddb9511f79a7e18bdaeb0c976f8255
euromak <euromak2016@yandex.ru>
2019-08-04 16:12:09 +0300

    Rewrite function getMakeRequest as promise construction

--- project/js/main.js
+++ project/js/main.js
@@ -1,31 +1,25 @@
 'use strict';
-/*const products = [
-  {id: 1, title: 'Куртка', price: 10000, image: 'https://www.topmangal.com/wp-content/uploads/man/product_1.jpg',},
-  {id: 2, title: 'Пальто', price: 10000, image: 'https://www.topmangal.com/wp-content/uploads/man/product_2.jpg',},
-  {id: 3, title: 'Куртка', price: 7500, image: 'https://www.topmangal.com/wp-content/uploads/man/product_3.jpg',},
-  {id: 4, title: 'Поло', price: 4500, image: 'https://www.topmangal.com/wp-content/uploads/man/product_4.jpg',},
-  {id: 5, title: 'Худи', price: 6000, image: 'https://www.topmangal.com/wp-content/uploads/man/product_5.jpg',},
-  {id: 6, title: 'Куртка', price: 7000, image: 'https://www.topmangal.com/wp-content/uploads/man/product_6.jpg',},
-  {id: 7, title: 'Пиджак', price: 5500, image: 'https://www.topmangal.com/wp-content/uploads/man/product_7.jpg',},
-  {id: 8, title: 'Куртка', price: 9000, image: 'https://www.topmangal.com/wp-content/uploads/man/product_8.jpg',},
-];
+const API = 'https://raw.githubusercontent.com/GeekBrainsTutorial/online-store-api/master/responses';
 
-const renderProduct = (title, price, image) => {
-  return `<div class="product-item">
-            <img src="${image}" alt="">
-            <h3>${title}</h3>
-            <p>${price} руб.</p>
-            <button class="by-btn">Добавить</button>
-          </div>`;
+// Переделать в ДЗ
+let makeGetRequest = (url) => {
+    return new Promise((resolve, reject) => {
+        let xhr = new XMLHttpRequest();
+        xhr.open('GET', url, true);
+        xhr.onreadystatechange = () => {
+            if (xhr.readyState === 4) {
+                if (xhr.status !== 200) {
+                    reject('Error');
+                } else {
+                    resolve(xhr.responseText);
+                }
+            }
+        };
+        xhr.send();
+    });
 };
 
-const renderProducts = list =>
-  document.querySelector('.products').innerHTML = list.map(item => renderProduct(item.title, item.price, item.image))
-      .join('');
-;
-
-renderProducts(products);
-*/
+makeGetRequest(`${API}/catalogData.json`).then(result => console.log(result)).catch(error => console.log(error));
 
 
 class ProductList {
