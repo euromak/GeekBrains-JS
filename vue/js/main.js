455859e8e4eea42cae9cffe0a9ecd235eea5b75c
euromak <euromak2016@yandex.ru>
2019-08-10 17:57:38 +0300

    Added addBasketProduct method

--- vue/js/main.js
+++ vue/js/main.js
@@ -8,33 +8,52 @@ const app = new Vue({
         catalogProducts: [],
         basketProducts: [],
         filterProducts: [],
-        image: 'https://placehold.it/200x150',
+        images: ['https://placehold.it/200x150', 'https://placehold.it/50x100'],
         message: 'Hello Viktor',
 
     },
 
     methods: {
-        getJson() {
-            return fetch(`${this.api}/catalogData.json`)
+        renderCatalog() {
+            const response =  fetch(`${this.api}/catalogData.json`)
                 .then(response => response.json())
                 .catch(error => console.log(error));
+
+            response.then(result => {
+                for(let key in result) {
+                    this.catalogProducts.push(result[key]);
+                    //console.log(this.catalogProducts);
+                }
+            })
         },
 
-        handlerData(data) {
-            for(let key in data) {
-                this.catalogProducts.push(data[key]);
-                //console.log(this.catalogProduct);
-            }
-        },
+        addToBasket() {
+            let productId = +event.target.dataset['id'];
 
-        init() {
-            this.getJson().then(result => this.handlerData(result));
-        },
+            let response = fetch(`${this.api}/addToBasket.json`).then(response => response.json());
+            response.then(data => {
+                if(data.result === 1) {
+                    let product = this.catalogProducts.find(product => product.id_product === productId);
+                    if(this.basketProducts.includes(product)) {
+                        product.quantity++;
+                        console.log(this.basketProducts);
+                    } else {
+                        product.quantity = 1;
+                        this.basketProducts.push(product);
+                        console.log(this.basketProducts);
+                    }
+                }
+            });
+        }
+
+    },
+
+    computed: {
 
     },
 
     mounted() {
-        this.init();
+        this.renderCatalog();
     },
 
 });
